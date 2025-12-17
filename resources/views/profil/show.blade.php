b7f2d6d0587e626b8b5f302580f582f03f74bfdb
Lou DE BAERE <lou_debaere@ens.univ-artois.fr>
2025-12-17 14:12:58 +0100

    ajout des abonnés et abonnements sur le profil

--- resources/views/profil/show.blade.php
+++ resources/views/profil/show.blade.php
@@ -1,11 +1,14 @@
 <x-layout.app title="Mon profil">
     <section>
+
+        {{-- navigation : retourner à l'accueil et modifier le profil --}}
         <div>
-            <a href="{{ route('accueil') }}">
-                Retour à l'accueil
-            </a>
+            <a href="{{ route('accueil') }}">Retour à l'accueil</a>
+            <br>
+            <a href="{{ route('profil.edit', $utilisateur->id) }}">Modifier mon profil</a>
         </div>
 
+        {{-- profil : avatar + infos du compte --}}
         <div>
             <div>
                 <img src="{{ $utilisateur->avatar }}"
@@ -14,8 +17,15 @@
             <div>
                 <h1>Nom : {{ $utilisateur->name }}</h1>
                 <p>Email : {{ $utilisateur->email }}</p>
-                <p>Date d'inscription : {{ $utilisateur->created_at }}</p>
+                <p>Membre depuis : {{ $utilisateur->created_at }}</p>
             </div>
         </div>
+
+        {{-- statistiques sur les followers --}}
+        <div>
+            <p>Abonnés : {{ $utilisateur->followers->count() }}</p>
+            <p>Abonnements : {{ $utilisateur->followings->count() }}</p>
+        </div>
+
     </section>
 </x-layout.app>
