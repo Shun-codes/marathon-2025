e4a1fb76a5830d1c91359b66be0771cc729c99f6
Shun Lembrez <shun_lembrez@ens.univ-artois.fr>
2025-12-17 19:47:16 +0100

    Ajout d'un critère d'affichage des articles dans la page d'accueil aussi via la fonction home dans le controller #15

--- resources/views/statiques/welcome.blade.php
+++ resources/views/statiques/welcome.blade.php
@@ -1,22 +1,21 @@
 <x-layout.app :title="'Page Accueil'">
 
-<section class="header">
-    <div>
-        <h1>BOSSA NOVA</h1>
-        <p id="text-header">Découvrez les derniers articles de ce style chaleureux </p>
-    </div>
-    <img id="fond-img" src="{{ Vite::asset('resources/images/logo.png') }}" alt="Logo du site" class="logo-header">
-</section>
+    <section class="header">
+        <div>
+            <h1>BOSSA NOVA</h1>
+            <p id="text-header">Découvrez les derniers articles de ce style chaleureux </p>
+        </div>
+        <img id="fond-img" src="{{ Vite::asset('resources/images/logo.png') }}" alt="Logo du site" class="logo-header">
+    </section>
 
-<section class="container">
-    <h1 class="titre">Les derniers Articles</h1>
-    
+    <section class="container">
+        <h1 class="titre">Les derniers Articles</h1>
 
-    <div class="articles">
-        @foreach($articles as $article)
-            <x-card-article :article="$article"/>
-        @endforeach
-    </div>
-</section>
+        <div class="articles">
+            @foreach($articles as $article)
+                <x-card-article :article="$article"/>
+            @endforeach
+        </div>
+    </section>
 
 </x-layout.app>
