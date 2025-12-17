3933a5bf3ef8f1100ea2ae570c1cf545f561e891
Romain <romain_thibaut1@ens.univ-artois.fr>
2025-12-17 11:46:02 +0100

    Redefinition de l'architecture des vues. Et création d'un composent navbar #18

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>{{isset($title) ? $title : "Page en cours"}}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+@vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<header>Ma super application</header>
+
+
+<main>
+    @yield("contenu")
+</main>
+
+<footer>IUT de Lens</footer>
+</body>
+</html>
