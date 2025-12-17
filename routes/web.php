6f175b63a15bd100fae42b61da13eb720f66877b
Lou DE BAERE <lou_debaere@ens.univ-artois.fr>
2025-12-17 12:37:29 +0100

    changement de l'emplacement de la page d'accueil dans le projet + changement pour la route adaptée

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/statiques/welcome');
 })->name("accueil");
 
 Route::get('/contact', function () {
