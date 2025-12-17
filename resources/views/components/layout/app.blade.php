7d26d4cddab526e8731fcba2f981c825d8bd9f26
Lucas Ternel <lucas.ternel62@gmail.com>
2025-12-17 16:20:15 +0100

    v1 header

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <title>{{isset($title) ? $title : "Page en cours"}}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://use.typekit.net/idi8qqz.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
 </head>
